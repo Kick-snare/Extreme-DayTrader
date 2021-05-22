47608a8a4d51130c622ae11469de243e1782812f
홍유준 <hyjhyj0901@pusan.ac.kr>
2021-05-22 23:06:31 +0900

    add other coin's info

--- public/javascripts/BTC.js
+++ public/javascripts/BTC.js
@@ -1,49 +1,49 @@
 $.ajaxSetup({cache:false});
 
-// function draw3(){
-//     var chartdata = [];
+function draw3(){
+    var chartdata = [];
 
-//     $.getJSON('http://localhost:3000/poloniex-api', (data) => {
-//         var chartapi = JSON.parse(data);
-//         $.each(chartapi, (i, item) => {
-//             chartdata.push([item.date*1000, item.open, item.high, item.low, item.close]);
-//         });
-//     }).done(function(){
-//         Highcharts.stockChart('chart',{
-//             title: {
-//                 text: 'BTC' + ' 차트 (5m)'
-//             },
-//             rangeSelector: {
-//                 buttons: [
-//                     {type: 'minute',count: 30,text: '30m'},
-//                     {type: 'hour',count: 1,text: '1h'},
-//                     {type: 'hour',count: 2,text: '2h'},
-//                     {type: 'hour',count: 6,text: '6h'},
-//                     {type: 'all',count: 1,text: '1d'}
-//                 ],
-//                 selected: 3,
-//                 inputEnabled: true
-//             },
-//             plotOptions: {
-//                 candlestick: {
-//                     downColor: 'blue',
-//                     upColor: 'red'
-//                 }
-//             },
-//             series: [{
-//                 name: 'BTC USDT($)',
-//                 type: 'candlestick',
-//                 data: chartdata,
-//                 tooltip: {
-//                     valueDecimals: 2
-//                 },
-//                 animation: {
-//                     duration: 2000
-//                 }
-//             }]
-//         });
-//     });
-// }
+    $.getJSON('http://localhost:3000/poloniex-api', (data) => {
+        var chartapi = JSON.parse(data);
+        $.each(chartapi, (i, item) => {
+            chartdata.push([item.date*1000, item.open, item.high, item.low, item.close]);
+        });
+    }).done(function(){
+        Highcharts.stockChart('chart',{
+            title: {
+                text: 'BTC' + ' 차트 (5m)'
+            },
+            rangeSelector: {
+                buttons: [
+                    {type: 'minute',count: 30,text: '30m'},
+                    {type: 'hour',count: 1,text: '1h'},
+                    {type: 'hour',count: 2,text: '2h'},
+                    {type: 'hour',count: 6,text: '6h'},
+                    {type: 'all',count: 1,text: '1d'}
+                ],
+                selected: 3,
+                inputEnabled: true
+            },
+            plotOptions: {
+                candlestick: {
+                    downColor: 'blue',
+                    upColor: 'red'
+                }
+            },
+            series: [{
+                name: 'BTC USDT($)',
+                type: 'candlestick',
+                data: chartdata,
+                tooltip: {
+                    valueDecimals: 2
+                },
+                animation: {
+                    duration: 2000
+                }
+            }]
+        });
+    });
+}
 
 
 function numberWithCommas(x) {
@@ -88,3 +88,27 @@ function proc() {
     setTimeout("proc()", 500);
 }
 
+function proc2() {
+    draw3();
+    setTimeout("proc()", 8000);
+}
+
+{/* <script>
+    new TradingView.widget(
+    {
+    "autosize": true,
+    "symbol": "BINANCE:BTCUSD",
+    "interval": "240",
+    "timezone": "Asia/Seoul",
+    "theme": "light",
+    "style": "1",
+    "locale": "kr",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "hide_top_toolbar": true,
+    "hide_legend": true,
+    "save_image": false,
+    "container_id": "tradingview_f6623"
+    }
+    );
+    </script> */}
